12cd924ae0093d264aa13b3e894f2da8be2672d4
Dato Protegido <demiancrate@gmail.com>
2026-03-10 16:47:19 -0600

    fix: change meses to semanas in restructure credit dropdown

--- admin/src/views/Modules/Credits/CreditRestructure.vue
+++ admin/src/views/Modules/Credits/CreditRestructure.vue
@@ -170,7 +170,7 @@
                     class="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary text-lg"
                   >
                     <option value="Tradicional">Crédito Tradicional</option>
-                    <option value="Personalizado">Crédito Personalizado (36 meses)</option>
+                    <option value="Personalizado">Crédito Personalizado (36 semanas)</option>
                   </select>
                 </div>
               </div>
